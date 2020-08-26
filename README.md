52fb1636f2ee36422690bac71907be438eb7dc5a
Gabor Bata <bata.gabor@gmail.com>
2020-08-26 16:51:40 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # todo
-todo list manager inspired by todo.txt using the jsonl format
+
+todo list manager inspired by [todo.txt](http://todotxt.org) using the [jsonl](http://jsonlines.org) format
+
+```
+Usage: todo <command> <arguments>
+
+Commands:
+* add <text>                     add new task
+* start <tasknumber>             mark task as started
+* done <tasknumber>              mark task as completed
+* block <tasknumber>             mark task as blocked
+
+* append <tasknumber> <text>     append text to task title
+* replace <tasknumber> <text>    replace task
+* del <tasknumber>               delete task
+* note <tasknumber> <text>       add note to task
+
+* list <regexp>                  list tasks (only not completed by default)
+* show <tasknumber>              show all task details
+* help                           this help screen
+```
+
+## Demo
+
+![todo](todo.gif)
+
+## Requirements
+
+Ruby 2.6 or newer
---
+++ todo.rb
@@ -0,0 +1,233 @@
+#!/usr/bin/env ruby
+
+# todo.rb - todo list manager inspired by todo.txt using the jsonl format.
+#
+# Copyright (c) 2020 Gabor Bata
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'json'
+
+DATE_FORMAT = '%Y-%m-%d'
+
+STATES = {
+  'new'  => '[ ]',
+  'done' => '[x]',
+  'started' => '[>]',
+  'blocked' => '[!]',
+  'default' => '[?]'
+}
+
+PRIO = {
+  'new'     => 3,
+  'done'    => 4,
+  'started' => 2,
+  'blocked' => 1,
+  'default' => 100
+}
+
+COLORS = {
+  'new'     => 37,
+  'done'    => 34,
+  'started' => 32,
+  'blocked' => 33,
+  'default' => 31
+}
+
+TODO_FILE = "#{ENV["HOME"]}/todo.jsonl"
+
+def usage
+  <<~USAGE
+    Usage: todo <command> <arguments>
+
+    Commands:
+    * add <text>                     add new task
+    * start <tasknumber>             mark task as started
+    * done <tasknumber>              mark task as completed
+    * block <tasknumber>             mark task as blocked
+
+    * append <tasknumber> <text>     append text to task title
+    * replace <tasknumber> <text>    replace task
+    * del <tasknumber>               delete task
+    * note <tasknumber> <text>       add note to task
+
+    * list <regexp>                  list tasks (only not completed by default)
+    * show <tasknumber>              show all task details
+    * help                           this help screen
+   USAGE
+end
+
+def get_tasks
+  count = 0
+  tasks = {}
+  if File.exist?(TODO_FILE)
+    File.open(TODO_FILE, 'r:UTF-8') do |file|
+      file.each_line do |line|
+        next if line.strip == ''
+        count += 1
+        tasks[count] = JSON.parse(line.chomp)
+      end
+    end
+  end
+  tasks
+end
+
+def write_tasks(tasks)
+  keys = tasks.keys.sort
+  file = File.open(TODO_FILE, 'w:UTF-8')
+  keys.each do |key|
+    file.write(JSON.generate(tasks[key]) + "\n")
+  end
+  file.close
+end
+
+def add(text)
+  file = File.open(TODO_FILE, 'a:UTF-8')
+  task = {
+    'state' => 'new',
+    'title' => text,
+    'modified' => Time.now.strftime(DATE_FORMAT)
+  }
+  file.write(JSON.generate(task) + "\n")
+  file.close
+  list
+end
+
+def append(item, text = '')
+  tasks = get_tasks
+  check_item(tasks, item)
+  tasks[item]['title'] = [tasks[item]['title'], text].join(' ')
+  write_tasks(tasks)
+  list(tasks)
+end
+
+def replace(item, text)
+  tasks = get_tasks
+  check_item(tasks, item)
+  tasks[item] = {
+    'state' => 'new',
+    'title' => text,
+    'modified' => Time.now.strftime(DATE_FORMAT)
+  }
+  write_tasks(tasks)
+  list(tasks)
+end
+
+def delete(item)
+  tasks = get_tasks
+  check_item(tasks, item)
+  tasks.delete(item)
+  write_tasks(tasks)
+  list
+end
+
+def change_state(item, state)
+  tasks = get_tasks
+  check_item(tasks, item)
+  tasks[item]['state'] = state
+  tasks[item]['modified'] = Time.now.strftime(DATE_FORMAT)
+  write_tasks(tasks)
+  list(tasks)
+end
+
+def list(tasks_map = nil, patterns = nil)
+  items = {}
+  tasks = tasks_map.nil? ? get_tasks : tasks_map
+  patterns = ["\"state\":\"(new|started|blocked)\""] if patterns.nil? || patterns.empty?
+  tasks.each do |num, task|
+    match = true
+    patterns.each do |pattern|
+      match = false unless /#{pattern}/ix.match(JSON.generate(task))
+    end
+    items[num] = task if match
+  end
+  items = items.sort_by do |num, task|
+    prio = PRIO[task['state'] || 'default']
+    [prio.to_s, num]
+  end
+  items.each do |num, task|
+    state = task['state'] || 'default'
+    color = COLORS[state]
+    display_state = "\e[#{color}m#{STATES[state]}\e[0m" 
+    title = task['title'].gsub(/#\w+/) { |tag| "\e[36m" + tag + "\e[0m" }
+    puts "#{num.to_s.rjust(4, ' ')}: #{display_state} #{title}"
+  end
+end
+
+def add_note(item, text)
+  tasks = get_tasks
+  check_item(tasks, item)
+  tasks[item]['note'] = [] if tasks[item]['note'].nil?
+  tasks[item]['note'].push(text)
+  write_tasks(tasks)
+  list(tasks)
+end
+
+def show(item)
+  tasks = get_tasks
+  check_item(tasks, item)
+  tasks[item].each do |key, value|
+    val = value.kind_of?(Array) ? "\n" + value.join("\n") : value
+    puts "#{key.to_s.rjust(10, ' ')}: #{val}"
+  end
+end
+
+def check_item(tasks, item)
+  unless tasks.has_key?(item)
+    puts "\e[31mERROR:\e[0m #{item}: No such todo"
+    exit
+  end
+end
+
+def read(arguments)
+  action = arguments.first
+  args = arguments[1..-1]
+
+  case action
+  when 'add'
+    add(args.join(' ')) unless args.nil?
+  when 'start'
+    change_state(args.first.to_i, 'started') if args.length == 1
+  when 'done'
+    change_state(args.first.to_i, 'done') if args.length == 1
+  when 'block'
+    change_state(args.first.to_i, 'blocked') if args.length == 1
+  when 'append'
+    append(args.first.to_i, args[1..-1].join(' ')) unless args.length < 1
+  when 'replace'
+    replace(args.first.to_i, args[1..-1].join(' ')) unless args.length < 1
+  when 'del'
+    delete(args.first.to_i) if args.length == 1
+  when 'note'
+    add_note(args.first.to_i, args[1..-1].join(' ')) unless args.length < 1
+  when 'list'
+    list(nil, args)
+  when 'show'
+    show(args.first.to_i) if args.length == 1
+  when 'help'
+    puts usage
+    exit
+  else
+    list(nil, arguments)
+  end
+end
+
+read(ARGV)
