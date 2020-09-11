f81547023b78f0a9a35c009440c5a66e2be7bc4f
Gabor Bata <bata.gabor@gmail.com>
2020-09-11 10:50:38 +0200

    Verify stdout in tests, add more test cases

--- test/test_todo.rb
+++ test/test_todo.rb
@@ -3,6 +3,7 @@ require 'test/unit'
 class TestTodo < Test::Unit::TestCase
 
   def setup
+    @original_stdout = $stdout
     $stdout = StringIO.new
     @todo_file = "#{Dir.pwd}/todo.jsonl"
     ENV['HOME'] = Dir.pwd
@@ -12,6 +13,7 @@ class TestTodo < Test::Unit::TestCase
   end
 
   def teardown
+    $stdout = @original_stdout
     File.delete(@todo_file) if File.exist?(@todo_file)
   end
 
@@ -20,79 +22,98 @@ class TestTodo < Test::Unit::TestCase
       /{"state":"new","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}"}\n/,
       File.read(@todo_file)
     )
+    assert_equal("   1: \e[37m[ ]\e[0m Buy Milk", $stdout.string.split("\n").last)
   end
 
   def test_start
+    $stdout = StringIO.new
     read ['start', '1']
     assert_match(
       /{"state":"started","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}"}\n/,
       File.read(@todo_file)
     )
+    assert_equal("   1: \e[32m[>]\e[0m Buy Milk\n", $stdout.string)
   end
 
   def test_start_not_existing_todo
+    $stdout = StringIO.new
     read ['start', '2']
     assert_match(
       /{"state":"new","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}"}\n/,
       File.read(@todo_file)
     )
+    assert_equal("\e[31mERROR:\e[0m 2: No such todo\n", $stdout.string)
   end
 
   def test_done
+    $stdout = StringIO.new
     read ['done', '1']
     assert_match(
       /{"state":"done","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}"}\n/,
       File.read(@todo_file)
     )
+    assert_equal("No todos found\n", $stdout.string)
   end
 
   def test_block
+    $stdout = StringIO.new
     read ['block', '1']
     assert_match(
       /{"state":"blocked","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}"}\n/,
       File.read(@todo_file)
     )
+    assert_equal("   1: \e[33m[!]\e[0m Buy Milk\n", $stdout.string)
   end
 
   def test_priority
+    $stdout = StringIO.new
     read ['prio', '1']
     assert_match(
       /{"state":"new","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}","priority":true}\n/,
       File.read(@todo_file)
     )
+    assert_equal("   1:\e[31m*\e[0m\e[37m[ ]\e[0m Buy Milk\n", $stdout.string)
   end
 
   def test_toggle_priority
     read ['prio', '1']
+    $stdout = StringIO.new
     read ['prio', '1']
     assert_match(
       /{"state":"new","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}","priority":false}\n/,
       File.read(@todo_file)
     )
+    assert_equal("   1: \e[37m[ ]\e[0m Buy Milk\n", $stdout.string)
   end
 
   def test_append
+    $stdout = StringIO.new
     read ['append', '1', 'ASAP']
     assert_match(
       /{"state":"new","title":"Buy Milk ASAP","modified":"\d{4}-\d{2}-\d{2}"}\n/,
       File.read(@todo_file)
     )
+    assert_equal("   1: \e[37m[ ]\e[0m Buy Milk ASAP\n", $stdout.string)
   end
 
   def test_rename_todo
+    $stdout = StringIO.new
     read ['rename', '1', 'Buy Bread']
     assert_match(
       /{"state":"new","title":"Buy Bread","modified":"\d{4}-\d{2}-\d{2}"}\n/,
       File.read(@todo_file)
     )
+    assert_equal("   1: \e[37m[ ]\e[0m Buy Bread\n", $stdout.string)
   end
 
   def test_delete_todo
+    $stdout = StringIO.new
     read ['del', '1']
     assert_equal(
       '',
       File.read(@todo_file)
     )
+    assert_equal("No todos found\n", $stdout.string)
   end
 
   def test_add_note
@@ -109,6 +130,35 @@ class TestTodo < Test::Unit::TestCase
       /{"state":"new","title":"Buy Milk","modified":"\d{4}-\d{2}-\d{2}","note":\[\]}\n/,
       File.read(@todo_file)
     )
+  end
+
+  def test_without_parameters
+    $stdout = StringIO.new
+    read []
+    assert_equal("   1: \e[37m[ ]\e[0m Buy Milk\n", $stdout.string)
+  end
+
+  def test_show_todo
+    $stdout = StringIO.new
+    read ['show', '1']
+    assert_match(
+      /\e\[36m {5}state:\e\[0m new\n\e\[36m {5}title:\e\[0m Buy Milk\n\e\[36m  modified:\e\[0m \d{4}-\d{2}-\d{2}\n/,
+      $stdout.string
+    )
+  end
+
+  def test_list_by_tag
+    read ['add', 'Buy Bread @breakfast']
+    $stdout = StringIO.new
+    read ['list', '@breakfast']
+    assert_equal("   2: \e[37m[ ]\e[0m Buy Bread \e[36m@breakfast\e[0m\n", $stdout.string)
+  end
+
+  def test_list_by_pre_defined_pattern
+    read ['done', '1']
+    $stdout = StringIO.new
+    read ['list', ':all']
+    assert_equal("   1: \e[34m[x]\e[0m Buy Milk\n", $stdout.string)
   end
 
 end
