8dce78ee610bc667fed72f0505eb5728562b8d73
Gabor Bata <bata.gabor@gmail.com>
2021-03-10 16:54:58 +0100

    Added package.json for the node version

--- node/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo Compile todo.js.rb for nodejs...
-opal --require date --require json --compile todo.js.rb | grep -v "//# sourceMappingURL" > todo.js
-echo Compress/optimize todo.js...
-java -jar closure-compiler.jar --isolation_mode IIFE --warning_level QUIET --js todo.js --js_output_file todo.min.js
-mv todo.min.js todo.js
